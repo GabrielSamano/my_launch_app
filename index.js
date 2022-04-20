3ac5c92a2dfa121dd17669607c99810ffed2b9d6
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-04-20 10:05:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const Gabriel = new MissionCommander("Gabriel")
+console.log(Gabriel.name)
+const Equipo = new MissionCommander("Bayern")
+console.log(Equipo.name)
+const Auto = new MissionCommander("Ferrari")
+console.log(Auto.name)
