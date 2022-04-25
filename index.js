678409dba7d98c4768ac2950989b9a314153cb92
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-04-25 13:36:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,4 @@ const Equipo = new MissionCommander("Bayern")
 console.log(Equipo.name)
 const Auto = new MissionCommander("Ferrari")
 console.log(Auto.name)
-const Edad = new MissionCommander(23)
-console.log(Edad.age)
+
